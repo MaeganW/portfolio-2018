eac4648eeb66cdeb41036dd682183904eb2291ec
MaeganW <maeganwomble@gmail.com>
2018-03-13 14:13:43 -0500

    added 8 more project images to the projects grid

--- src/components/projects/ProjectsComponent.jsx
+++ src/components/projects/ProjectsComponent.jsx
@@ -10,8 +10,33 @@ import scroll from '../../img/projects/Scroll.png';
 import shiftSelect from '../../img/projects/Shift Select Checkboxes.png';
 import typeAhead from '../../img/projects/Type Ahead.png';
 
+import colorGame from '../../img/projects/ColorGame.png';
+import gamma from '../../img/projects/Gamma.png';
+import metaVidz from '../../img/projects/MetaVidz.png';
+import okCrime from '../../img/projects/OKCrime.png';
+import pomodoro from '../../img/projects/Pomodoro.png';
+import todo from '../../img/projects/Todo.png';
+import trillo from '../../img/projects/Trillo.png';
+import patatap from '../../img/projects/Patatap.png';
+
 
 const projectImgs = [
+  {
+    src: trillo,
+    href: 'http://google.com'
+  },
+  {
+    src: colorGame,
+    href: 'http://google.com'
+  },
+  {
+    src: gamma,
+    href: 'http://google.com'
+  },
+  {
+    src: metaVidz,
+    href: 'http://google.com'
+  },
   {
     src: jsClock,
     href: 'http://google.com'
@@ -41,7 +66,23 @@ const projectImgs = [
     href: 'http://google.com'
   },
   {
+    src: patatap,
+    href: 'http://google.com'
+  },
+  {
     src: typeAhead,
+    href: 'http://google.com'
+  },
+  {
+    src: pomodoro,
+    href: 'http://google.com'
+  },
+  {
+    src: okCrime,
+    href: 'http://google.com'
+  },
+  {
+    src: todo,
     href: 'http://google.com'
   }
 ];
