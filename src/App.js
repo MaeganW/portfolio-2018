6ea783c7408b24332e942d1ff994b27757bfdc44
MaeganW <maeganwomble@gmail.com>
2018-03-16 20:43:54 -0500

    about section placeholder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from './components/home/HomeComponent';
 import Projects from './components/projects/ProjectsComponent';
+import AboutComponent from './components/about/AboutComponent';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ class App extends Component {
     return (
       <div className="App">
         <Home></Home>
+        <AboutComponent></AboutComponent>
         <Projects></Projects>
       </div>
     );
