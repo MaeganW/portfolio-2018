540baefe6376a590849d2f6d0f731c7258d29336
MaeganW <maeganwomble@gmail.com>
2018-03-16 20:28:04 -0500

    about component started

---
+++ src/components/about/AboutComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutComponent = () => (
+  <div className="about">
+    <div className="about_content">
+      <p>This will be something awesome.</p>
+    </div>
+  </div>
+);
+
+export default AboutComponent;
